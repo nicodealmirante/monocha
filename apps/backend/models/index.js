0ab73344af6b40a88d719ba7ef2cf94c984b3013
nicodealmirante <nicolas.e.luna@gmail.com>
2025-12-09 10:43:50 -0300

    Update index.js

--- apps/backend/models/index.js
+++ apps/backend/models/index.js
@@ -1,4 +1,6 @@
-// Bridge file para reexportar modelos y sequelize
-const models = require('../../packages/common/models');
+// apps/backend/models/index.js
+
+// Sube tres niveles: ../../../  y luego entra a packages/common/models
+const models = require('../../../packages/common/models');
 
 module.exports = models;
