2863ae7ba5a04fc8aec925a9688197a13c67a07e
nicodealmirante <nicolas.e.luna@gmail.com>
2025-12-09 08:35:53 -0300

    Update config.js

--- apps/frontend/public/config.js
+++ apps/frontend/public/config.js
@@ -1,6 +1,6 @@
 const DEFAULT_API_BASE =
   typeof window !== "undefined"
     ? window.location.origin.replace(/\/$/, "") // mismo dominio que el front (Railway)
-    : process.env.API_BASE || "http://localhost:3000"; // fallback para desarrollo
+    : process.env.API_BASE || "http://localhost:3001"; // fallback para desarrollo
 
 export { DEFAULT_API_BASE };
